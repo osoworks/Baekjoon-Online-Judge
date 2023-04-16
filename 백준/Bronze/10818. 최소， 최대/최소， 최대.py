2e8408dde1ee88349266e975c4fde411d04b6ecf
Junga Esther Kim <89962180+osoworks@users.noreply.github.com>
2023-04-16 20:30:54 +0900

    [Bronze III] Title: 최소, 최대, Time: 384 ms, Memory: 155720 KB -BaekjoonHub

--- 백준/Bronze/10818. 최소， 최대/최소， 최대.py
+++ 백준/Bronze/10818. 최소， 최대/최소， 최대.py
@@ -1,5 +1,20 @@
 x = int(input())
 y = list(map(int, input().split()))
 
-z = sorted(y)
-print(z[0], z[-1])
+#방법 1
+print(min(y), max(y))
+
+#방법 2
+#a = y[0] #최소값
+#b = y[0] #최대값
+
+#for i in y: 
+#for i in range(x): 로 대체 가능
+#	if i > a:
+#		a = i
+#	elif i < min:
+#		b = i
+
+#방법 3
+#z = sorted(y)
+#print(z[0], z[-1])
